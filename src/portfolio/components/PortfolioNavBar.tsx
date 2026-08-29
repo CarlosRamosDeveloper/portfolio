224d32f4448faaac4cf06599fc1a56c18639a323
Carlos Ramos <carlosramosdev@protonmail.com>
2026-08-29 16:40:32 +0200

    implemented experience page

--- src/portfolio/components/PortfolioNavBar.tsx
+++ src/portfolio/components/PortfolioNavBar.tsx
@@ -4,6 +4,9 @@ export const PortfolioNavBar = () => {
   return (
     <div className="flex justify-center mb-3 gap-6">
       <div>
+        <NavigateButton path="experience" label="Experiencia y Formación" />
+      </div>
+      <div>
         <NavigateButton path="projects" label="Ver Proyectos" />
       </div>
       <div>
