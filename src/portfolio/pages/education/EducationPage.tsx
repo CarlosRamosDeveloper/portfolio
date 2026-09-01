aa6afbc87ea9c82d399cc34ff54f7b490666a986
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-01 17:46:41 +0200

    Updated education page text

--- src/portfolio/pages/education/EducationPage.tsx
+++ src/portfolio/pages/education/EducationPage.tsx
@@ -3,7 +3,7 @@ import { EducationCardList } from './components';
 export const EducationPage = () => {
   return (
     <div>
-      <h2 className="flex flex-1 text-center">Página de formación</h2>
+      <h2 className="flex flex-1 text-center">Formación Académica</h2>
       <EducationCardList />
     </div>
   );
