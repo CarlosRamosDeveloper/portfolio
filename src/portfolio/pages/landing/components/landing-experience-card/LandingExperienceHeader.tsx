8287f544e0601702929e86e3e874051989a94773
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-04 11:54:48 +0200

    minor cn fix

--- src/portfolio/pages/landing/components/landing-experience-card/LandingExperienceHeader.tsx
+++ src/portfolio/pages/landing/components/landing-experience-card/LandingExperienceHeader.tsx
@@ -10,9 +10,9 @@ export const LandingExperienceHeader = ({ title, subtitle, type }: Props) => {
   return (
     <div
       className={cn(
-        `flex items-center justify-between border-b border-border px-4 py-2 font-semibold 
-        ${type === 'job' ? 'bg-card-details' : ''} 
-        ${type === 'education' ? 'bg-card-secondary' : ''}`,
+        `flex items-center justify-between border-b border-border px-4 py-2 font-semibold`,
+        type === 'job' && 'bg-card-details',
+        type === 'education' && 'bg-card-secondary',
       )}
     >
       {title}
