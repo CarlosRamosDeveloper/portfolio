ef7f276f27b925b73d1183a24e907cdbc000aa80
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-04 17:36:27 +0200

    implemented navbar constants

--- src/portfolio/layout/components/PortfolioNavBar.tsx
+++ src/portfolio/layout/components/PortfolioNavBar.tsx
@@ -1,23 +1,27 @@
 import { ROUTES } from '@/constants/routes';
 import { NavigateButton } from '.';
+import { NAVBAR_TEXT } from '@/constants';
 
 export const PortfolioNavBar = () => {
   return (
     <div className="flex justify-center mb-3 gap-6">
       <div>
-        <NavigateButton path={ROUTES.experience} label="Experiencia Laboral" />
+        <NavigateButton
+          path={ROUTES.experience}
+          label={NAVBAR_TEXT.experience}
+        />
       </div>
       <div>
-        <NavigateButton path={ROUTES.education} label="Formación" />
+        <NavigateButton path={ROUTES.education} label={NAVBAR_TEXT.education} />
       </div>
       <div>
-        <NavigateButton path={ROUTES.featured} label="Proyectos Destacados" />
+        <NavigateButton path={ROUTES.featured} label={NAVBAR_TEXT.featured} />
       </div>
       <div>
-        <NavigateButton path={ROUTES.about} label="Acerca de" />
+        <NavigateButton path={ROUTES.about} label={NAVBAR_TEXT.about} />
       </div>
       <div>
-        <NavigateButton path={ROUTES.contact} label="Contacto" />
+        <NavigateButton path={ROUTES.contact} label={NAVBAR_TEXT.contact} />
       </div>
     </div>
   );
