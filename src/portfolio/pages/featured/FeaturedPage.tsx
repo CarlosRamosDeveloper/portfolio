a198ee72d6dbe0f0004bb36568547fe865615141
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-04 15:48:29 +0200

    removed non featured projects from featured projects

--- src/portfolio/pages/featured/FeaturedPage.tsx
+++ src/portfolio/pages/featured/FeaturedPage.tsx
@@ -1,13 +1,15 @@
 import { ProjectsData } from '@/portfolio/data/ProjectsData';
 import { FeaturedProjectsList } from './components';
 
+const data = ProjectsData.filter((project) => project.isFeatured).reverse();
+
 export const FeaturedPage = () => {
   return (
     <div className="w-full">
       <h2 className="text-center text-2xl font-semibold">
         Proyectos Destacados
       </h2>
-      <FeaturedProjectsList projects={ProjectsData} />
+      <FeaturedProjectsList projects={data} />
     </div>
   );
 };
