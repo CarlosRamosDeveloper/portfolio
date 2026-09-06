7e105d82b0af7769f506d51fb0ef287ca3c7b207
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-06 13:26:15 +0200

    Implemented background card list at education page

--- src/portfolio/pages/education/EducationPage.tsx
+++ src/portfolio/pages/education/EducationPage.tsx
@@ -1,5 +1,17 @@
+import { EducationData } from '@/portfolio/data';
+import type { Background } from '../components/BackgroundCard';
 import { EducationCardList } from './components';
 import { EDUCATION_TEXT } from '@/constants/pages';
+import { BackgroundCardsList } from '../components';
+
+const data: Background[] = [
+  ...EducationData.sort()
+    .reverse()
+    .map((experience) => ({
+      type: 'education' as const,
+      data: experience,
+    })),
+];
 
 export const EducationPage = () => {
   return (
@@ -8,6 +20,7 @@ export const EducationPage = () => {
         {EDUCATION_TEXT.title}
       </h2>
       <EducationCardList />
+      <BackgroundCardsList background={data} />
     </div>
   );
 };
