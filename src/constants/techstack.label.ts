392f755080dd275b35212ec8a3cdac1166712952
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-06 10:50:23 +0200

    implemented expand and collapse

---
+++ src/constants/techstack.label.ts
@@ -0,0 +1,6 @@
+export const TECHSTACK_LABEL = {
+  viewLessButton: 'Ver más',
+  oneTechRemaining: 'Ver la tecnología restante',
+  moreTechRemainingStart: 'Ver las otras',
+  moreTechRemainingEnd: 'tecnologías.',
+};
--- src/portfolio/pages/components/TechStackList.tsx
+++ src/portfolio/pages/components/TechStackList.tsx
@@ -1,18 +1,29 @@
 import type { Technology } from '@/interfaces';
 import { Separator, TechCard } from '.';
+import { useState } from 'react';
+import { TECHSTACK_LABEL } from '@/constants';
 
 interface Props {
   techStack: Technology[];
 }
 
+const techMaxValue = 5;
+
 export const TechStackList = ({ techStack }: Props) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   if (techStack.length === 0) return;
+
+  const visibleTech = isExpanded ? techStack : techStack.slice(0, techMaxValue);
+  const remainingTech = techStack.length - techMaxValue;
+  const onlyRemainingOneTech = remainingTech === 1;
+
   return (
     <>
       <Separator />
       <div className="p-1 bg-card-details">
         <div className="flex flex-wrap gap-2 m-0.5 p-1 justify-between">
-          {techStack.map((tech) => (
+          {visibleTech.map((tech) => (
             <TechCard
               key={tech.name}
               techName={tech.name}
@@ -21,6 +32,19 @@ export const TechStackList = ({ techStack }: Props) => {
             />
           ))}
         </div>
+
+        {techStack.length > techMaxValue && (
+          <button
+            type="button"
+            onClick={() => setIsExpanded((previous) => !previous)}
+          >
+            {isExpanded
+              ? TECHSTACK_LABEL.viewLessButton
+              : onlyRemainingOneTech
+                ? TECHSTACK_LABEL.oneTechRemaining
+                : `${TECHSTACK_LABEL.moreTechRemainingStart} ${remainingTech} ${TECHSTACK_LABEL.moreTechRemainingEnd}`}
+          </button>
+        )}
       </div>
     </>
   );
