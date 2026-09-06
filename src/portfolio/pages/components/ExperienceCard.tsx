e1366c72facaa139223d3ef1f8b3b7690094c0b8
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-06 12:35:13 +0200

    created experience card

---
+++ src/portfolio/pages/components/ExperienceCard.tsx
@@ -0,0 +1,19 @@
+export const ExperienceCard = () => {
+  return (
+    <div className="my-6 w-full max-w-3xl border border-foreground rounded-2xl bg-card text-card-foreground overflow-hidden">
+      <div className="flex p-3 justify-between">
+        {/* education: Titulo // job: Empresa */}
+        {/* Date label */}
+      </div>
+      {/* job: working possition */}
+      {/* job: experience subtitle */}
+      <div className="flex flex-1 justify-between">
+        {/* education: Centro educativo */}
+        {/* education:Tipo de educación */}
+      </div>
+      {/* Tech stack */}
+      {/* Descripción */}
+      {/* education: Proyectos */}
+    </div>
+  );
+};
