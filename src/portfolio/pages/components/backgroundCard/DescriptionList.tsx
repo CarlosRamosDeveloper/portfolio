5d7597759cd11ce1ef884fda68586a2c262eacbf
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-07 21:26:36 +0200

    Implemented toggling

--- src/portfolio/pages/components/backgroundCard/DescriptionList.tsx
+++ src/portfolio/pages/components/backgroundCard/DescriptionList.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { DescriptionLabel } from '.';
 import { Separator } from '..';
 
@@ -5,15 +6,32 @@ interface Props {
   descriptions: string[];
 }
 
+const longDescriptionLines = 3;
+
 export const DescriptionList = ({ descriptions }: Props) => {
   if (descriptions.length === 0) return null;
+
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const hasLongDescription = descriptions.length > longDescriptionLines;
+  const visibleDescription = isExpanded
+    ? descriptions.slice(0, longDescriptionLines)
+    : descriptions;
+
+  const handleToggleVisibility = () => {
+    setIsExpanded((prev) => !prev);
+  };
+
   return (
     <>
       <Separator />
       <div className="py-2 bg-card-secondary">
-        {descriptions.map((description, index) => (
+        {visibleDescription.map((description, index) => (
           <DescriptionLabel description={description} key={index} />
         ))}
+        {hasLongDescription && (
+          <button onClick={handleToggleVisibility}>Test</button>
+        )}
       </div>
     </>
   );
