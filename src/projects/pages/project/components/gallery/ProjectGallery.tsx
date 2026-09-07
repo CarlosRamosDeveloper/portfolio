b20088490529e257057888743f9159d9eebdf655
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-07 11:00:35 +0200

    implemented back and next buttons

--- src/projects/pages/project/components/gallery/ProjectGallery.tsx
+++ src/projects/pages/project/components/gallery/ProjectGallery.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { ProjectImageList } from './ProjectImageList';
 
 interface Props {
@@ -6,9 +7,48 @@ interface Props {
 }
 
 export const ProjectGallery = ({ images }: Props) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  if (images === null || images.length === 0) return null;
+
+  const currentImage = images[currentIndex];
+
+  const handlePrevItem = () => {
+    setCurrentIndex((index) => (index === 0 ? images.length - 1 : index - 1));
+  };
+
+  const handleNextItem = () => {
+    setCurrentIndex((index) => (index === images.length - 1 ? 0 : index + 1));
+  };
+
   return (
-    <div>
-      <ProjectImageList images={images} />
+    // <div>
+    //   <ProjectImageList images={images} />
+    // </div>
+
+    <div className="w-full">
+      {/* Imagen principal */}
+
+      <div className="flex h-100 items-center justify-center">
+        <img
+          className="max-h-full max-w-full object-contain"
+          src={currentImage}
+          alt={currentImage}
+        />
+      </div>
+
+      {/* Slider */}
+      <div className="mt-4 flex items-center gap-2">
+        <button type="button" onClick={handlePrevItem}>
+          {'<-'}
+        </button>
+        <button>
+          <img></img>
+        </button>
+        <button type="button" onClick={handleNextItem}>
+          {'->'}
+        </button>
+      </div>
     </div>
   );
 };
