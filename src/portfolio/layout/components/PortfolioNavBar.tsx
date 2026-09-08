299657c5bb5ca0a48838f3aab0d24b43731a10f5
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-08 14:59:04 +0200

    implemented styles

--- src/portfolio/layout/components/PortfolioNavBar.tsx
+++ src/portfolio/layout/components/PortfolioNavBar.tsx
@@ -5,7 +5,7 @@ import { NavigateButton } from '@/components';
 
 export const PortfolioNavBar = () => {
   return (
-    <div className="flex justify-center mb-3 gap-6">
+    <div className="flex justify-center mb-3 gap-3">
       <NavigateButton path={ROUTES.softSkills} label={NAVBAR_TEXT.softSkills} />
       <NavigateButton path={ROUTES.experience} label={NAVBAR_TEXT.experience} />
       <NavigateButton path={ROUTES.education} label={NAVBAR_TEXT.education} />
--- src/projects/layout/components/ProjectsNavBar.tsx
+++ src/projects/layout/components/ProjectsNavBar.tsx
@@ -4,7 +4,7 @@ import { NAVBAR_TEXT } from '@/constants/layout';
 
 export const ProjectsNavBar = () => {
   return (
-    <div className="flex justify-center mb-3 gap-6">
+    <div className="flex justify-center mb-3 gap-4">
       <NavigateButton label={NAVBAR_TEXT.projects} path={ROUTES.projects} />
       <NavigateButton label={NAVBAR_TEXT.featured} path={ROUTES.featured} />
       <div>[Buscador]</div>
