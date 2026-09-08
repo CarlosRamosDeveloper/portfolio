416b10f90c539edec5ebfcc4addf9d175da67aac
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-08 11:29:01 +0200

    improved internal navigation

--- src/portfolio/pages/featured/components/FeaturedNavigation.tsx
+++ src/portfolio/pages/featured/components/FeaturedNavigation.tsx
@@ -1,4 +1,5 @@
 import { handleNavigateOnNewWindow } from '@/portfolio/shared';
+import { Link } from 'react-router';
 
 type ItemType = 'page' | 'repository' | 'demo';
 
@@ -8,21 +9,27 @@ interface Props {
 }
 
 const COMPONENT_TEXT: Record<ItemType, string> = {
-  page: 'Acceder al proyecto',
+  page: 'Ver el proyecto al detalle',
   repository: 'Acceder al repositorio',
   demo: 'Ver demo',
 };
+
+const style = 'cursor-pointer p-3';
 
 export const FeaturedNavigation = ({ url, type }: Props) => {
   if (!url) return null;
   if (!type) return null;
 
+  if (type !== 'page')
+    return (
+      <div onClick={() => handleNavigateOnNewWindow(url)} className={style}>
+        {COMPONENT_TEXT[type]}
+      </div>
+    );
+
   return (
-    <div
-      onClick={() => handleNavigateOnNewWindow(url)}
-      className="cursor-pointer p-3"
-    >
+    <Link className={style} to={url}>
       {COMPONENT_TEXT[type]}
-    </div>
+    </Link>
   );
 };
