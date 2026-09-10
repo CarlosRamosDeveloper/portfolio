5c8f998dd824276fe4b2fb4910bc25cf93fe1f15
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-10 20:29:11 +0200

    implemented styles at landing

--- src/portfolio/pages/landing/components/LandingAbout.tsx
+++ src/portfolio/pages/landing/components/LandingAbout.tsx
@@ -5,13 +5,13 @@ import { LandingLabel } from './LandingLabel';
 
 export const LandingAbout = () => {
   return (
-    <div className="my-6 flex flex-col text-center">
+    <div className="my-1 lg:my-6 flex flex-col text-center">
       {LANDING_TEXT.description.map((text, index) => (
-        <div className="mt-3 p-2" key={index}>
+        <div className="mt-0.5 lg:mt-3 p-4 lg:p-2" key={index}>
           <LandingLabel text={text} type="description" />
         </div>
       ))}
-      <Link className="my-2" to={ROUTES.about}>
+      <Link className="lg:my-2" to={ROUTES.about}>
         <LandingLabel text={`[${LANDING_TEXT.aboutMe}]`} type="navigation" />
       </Link>
     </div>
