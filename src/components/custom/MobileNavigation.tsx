051338936c4e735acb74a5ffacc7fa6466abc282
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-10 19:06:06 +0200

    refactor

--- src/components/custom/MobileNavigation.tsx
+++ src/components/custom/MobileNavigation.tsx
@@ -1,54 +1,11 @@
 import { useState } from 'react';
+
 import { GiHamburgerMenu } from 'react-icons/gi';
 
-import { ROUTES } from '@/constants';
-import type { HamburgerNavigationItem } from '@/interfaces';
 import { MobileNavigationLinkList } from './MobileNavigationLinkList';
+import { navigationLinksData } from '@/data';
 
-const navigationLinks: HamburgerNavigationItem[] = [
-  {
-    title: 'Portfolio',
-    items: [
-      {
-        label: 'About',
-        path: ROUTES.about,
-      },
-      {
-        label: 'Experience',
-        path: ROUTES.experience,
-      },
-      {
-        label: 'Education',
-        path: ROUTES.education,
-      },
-      {
-        label: 'Contact',
-        path: ROUTES.contact,
-      },
-      {
-        label: 'Softskills',
-        path: ROUTES.softSkills,
-      },
-    ],
-  },
-  {
-    title: 'Projects',
-    items: [
-      {
-        label: 'All-Projects',
-        path: ROUTES.projects,
-      },
-      {
-        label: 'Featured',
-        path: ROUTES.featured,
-      },
-      {
-        label: 'search',
-        path: ROUTES.search,
-      },
-    ],
-  },
-];
+const navigationLinks = navigationLinksData;
 
 export const MobileNavigation = () => {
   const [isOpen, setIsOpen] = useState(false);
