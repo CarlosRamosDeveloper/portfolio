6fc92338dac2cf3efa358dd41bec99b1bc266a32
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-10 19:15:08 +0200

    fixed key issues at landing about

--- src/portfolio/pages/landing/components/LandingAbout.tsx
+++ src/portfolio/pages/landing/components/LandingAbout.tsx
@@ -5,17 +5,15 @@ import { LandingLabel } from './LandingLabel';
 
 export const LandingAbout = () => {
   return (
-    <div>
-      <div className="my-6 flex flex-col text-center">
-        {LANDING_TEXT.description.map((text) => (
-          <div className="mt-3 p-2">
-            <LandingLabel text={text} type="description" />
-          </div>
-        ))}
-        <Link className="my-2" to={ROUTES.about}>
-          <LandingLabel text={`[${LANDING_TEXT.aboutMe}]`} type="navigation" />
-        </Link>
-      </div>
+    <div className="my-6 flex flex-col text-center">
+      {LANDING_TEXT.description.map((text, index) => (
+        <div className="mt-3 p-2" key={index}>
+          <LandingLabel text={text} type="description" />
+        </div>
+      ))}
+      <Link className="my-2" to={ROUTES.about}>
+        <LandingLabel text={`[${LANDING_TEXT.aboutMe}]`} type="navigation" />
+      </Link>
     </div>
   );
 };
