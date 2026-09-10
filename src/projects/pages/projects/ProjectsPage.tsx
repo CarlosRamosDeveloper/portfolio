5a566ec660e2606cb90c9d370a37f150923aa8e3
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-10 14:45:34 +0200

    implemented frame in projects section

--- src/projects/pages/projects/ProjectsPage.tsx
+++ src/projects/pages/projects/ProjectsPage.tsx
@@ -2,13 +2,14 @@ import type { Project } from '@/interfaces';
 
 import { ProjectsData } from '@/data/ProjectsData';
 import { ProjectSummaryList } from './components';
+import { FramePage } from '@/components';
 
 const projectsList: Project[] = ProjectsData.sort().reverse();
 
 export const ProjectsPage = () => {
   return (
-    <div>
+    <FramePage>
       <ProjectSummaryList projects={projectsList} />
-    </div>
+    </FramePage>
   );
 };
