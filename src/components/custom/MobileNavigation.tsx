274ec3bf31c2a1e784b92ee8e3737e7db4501a34
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-10 17:56:13 +0200

    Implemented mobile navigation

--- src/components/custom/MobileNavigation.tsx
+++ src/components/custom/MobileNavigation.tsx
@@ -1,7 +1,69 @@
+import { ROUTES } from '@/constants';
+import { NavLink } from 'react-router';
+
 interface Props {
   section: 'portfolio' | 'projects';
 }
 
+const navigationSections = [
+  {
+    title: 'Portfolio',
+    items: [
+      {
+        label: 'About',
+        path: ROUTES.about,
+      },
+      {
+        label: 'Experience',
+        path: ROUTES.experience,
+      },
+      {
+        label: 'Education',
+        path: ROUTES.education,
+      },
+      {
+        label: 'Contact',
+        path: ROUTES.contact,
+      },
+      {
+        label: 'Softskills',
+        path: ROUTES.softSkills,
+      },
+    ],
+  },
+  {
+    title: 'Projects',
+    items: [
+      {
+        label: 'All-Projects',
+        path: ROUTES.projects,
+      },
+      {
+        label: 'Featured',
+        path: ROUTES.featured,
+      },
+      {
+        label: 'search',
+        path: ROUTES.search,
+      },
+    ],
+  },
+];
+
 export const MobileNavigation = ({ section }: Props) => {
-  return <div className="bg-accent p-5 lg:hidden">{section}</div>;
+  return (
+    <div className="bg-accent p-5 lg:hidden">
+      {navigationSections.map((section) => (
+        <div key={section.title}>
+          <h2>{section.title}</h2>
+
+          {section.items.map((item) => (
+            <NavLink key={item.path} to={item.path}>
+              {item.label}
+            </NavLink>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
