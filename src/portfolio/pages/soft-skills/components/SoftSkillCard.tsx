cc256cbb2fc394d97fd8dbdbcb94591d20a8d39e
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-10 15:09:52 +0200

    removed unnecesary styles from softskills section

--- src/portfolio/pages/soft-skills/components/SoftSkillCard.tsx
+++ src/portfolio/pages/soft-skills/components/SoftSkillCard.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export const SoftSkillCard = ({ skill }: Props) => {
   return (
-    <div className="flex flex-col mx-auto max-w-3xl mt-3 border border-border">
+    <div className="flex flex-col mt-3 border border-border">
       <div className="text-2xl text-center py-2 bg-card">{skill.name}</div>
       <div className="text-base px-4 py-2 bg-accent">{skill.description}</div>
     </div>
