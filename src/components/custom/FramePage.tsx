371393d8bc34c61defd6ee13b155cf2489996dab
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-10 16:37:36 +0200

    Implemented basic responsive design at framepage

--- src/components/custom/FramePage.tsx
+++ src/components/custom/FramePage.tsx
@@ -2,6 +2,6 @@ import { type PropsWithChildren } from 'react';
 
 export const FramePage = ({ children }: PropsWithChildren) => {
   return (
-    <div className="w-full mx-auto items-center max-w-3xl">{children}</div>
+    <div className="mx-auto w-full max-w-3xl p-4 sm:p-2 lg:p-0">{children}</div>
   );
 };
