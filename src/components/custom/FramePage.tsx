7c36b1e8b8be793a9d4f1157bf85a32885c53f8e
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-10 15:15:26 +0200

    removed bg color at FramePage

--- src/components/custom/FramePage.tsx
+++ src/components/custom/FramePage.tsx
@@ -2,8 +2,6 @@ import { type PropsWithChildren } from 'react';
 
 export const FramePage = ({ children }: PropsWithChildren) => {
   return (
-    <div className="w-full bg-accent mx-auto items-center max-w-3xl">
-      {children}
-    </div>
+    <div className="w-full mx-auto items-center max-w-3xl">{children}</div>
   );
 };
