4e87e2ff3088d766416d3bab8afe14f471c8e7c8
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-11 14:44:34 +0200

    updated basename for router

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -18,55 +18,60 @@ import {
   SearchPage,
 } from '@/projects/pages';
 
-export const appRouter = createBrowserRouter([
+export const appRouter = createBrowserRouter(
+  [
+    {
+      path: ROUTES.home,
+      element: <PorfolioLayout />,
+      children: [
+        {
+          index: true,
+          element: <LandingPage />,
+        },
+        {
+          path: ROUTES.about,
+          element: <AboutPage />,
+        },
+        {
+          path: ROUTES.experience,
+          element: <ExperiencePage />,
+        },
+        {
+          path: ROUTES.education,
+          element: <EducationPage />,
+        },
+        {
+          path: ROUTES.contact,
+          element: <ContactPage />,
+        },
+        {
+          path: ROUTES.softSkills,
+          element: <SoftSkillsPage />,
+        },
+      ],
+    },
+    {
+      path: ROUTES.projects,
+      element: <ProjectsLayout />,
+      children: [
+        { path: ROUTES.projects, element: <ProjectsPage /> },
+        {
+          path: ROUTES.featured,
+          element: <FeaturedPage />,
+        },
+        { path: ROUTES.projectId, element: <ProjectPage /> },
+        {
+          path: ROUTES.search,
+          element: <SearchPage />,
+        },
+      ],
+    },
+    {
+      path: '*',
+      element: <Navigate to="/" />,
+    },
+  ],
   {
-    path: ROUTES.home,
-    element: <PorfolioLayout />,
-    children: [
-      {
-        index: true,
-        element: <LandingPage />,
-      },
-      {
-        path: ROUTES.about,
-        element: <AboutPage />,
-      },
-      {
-        path: ROUTES.experience,
-        element: <ExperiencePage />,
-      },
-      {
-        path: ROUTES.education,
-        element: <EducationPage />,
-      },
-      {
-        path: ROUTES.contact,
-        element: <ContactPage />,
-      },
-      {
-        path: ROUTES.softSkills,
-        element: <SoftSkillsPage />,
-      },
-    ],
+    basename: import.meta.env.BASE_URL,
   },
-  {
-    path: ROUTES.projects,
-    element: <ProjectsLayout />,
-    children: [
-      { path: ROUTES.projects, element: <ProjectsPage /> },
-      {
-        path: ROUTES.featured,
-        element: <FeaturedPage />,
-      },
-      { path: ROUTES.projectId, element: <ProjectPage /> },
-      {
-        path: ROUTES.search,
-        element: <SearchPage />,
-      },
-    ],
-  },
-  {
-    path: '*',
-    element: <Navigate to="/" />,
-  },
-]);
+);
