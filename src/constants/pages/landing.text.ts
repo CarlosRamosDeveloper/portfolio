da26033a9ff43d3bb7190ec4e2d5a17e8b897abe
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-13 15:24:23 +0200

    Changed about me text at landing

--- src/constants/pages/landing.text.ts
+++ src/constants/pages/landing.text.ts
@@ -9,7 +9,7 @@ export const LANDING_TEXT = {
     'Soy desarrollador de software orientado al aprendizaje continuo y a la resolución de problemas. Mi formación combina experiencia profesional, formación especializada y aprendizaje autodidacta, con especial interés en Java, React y el desarrollo de aplicaciones.',
     'Disfruto enfrentándome a problemas nuevos, entender cómo funcionan las cosas y transformar lo aprendido en proyectos reales. Actualmente sigo ampliando y consolidando mis conocimientos mediante proyectos propios y formación continua.',
   ],
-  aboutMe: 'Aquí puedes ver más acerca de mi',
+  aboutMe: 'Ver más acerca de mi',
   education: 'Educación',
   work: 'Trabajo',
   internship: 'Prácticas',
