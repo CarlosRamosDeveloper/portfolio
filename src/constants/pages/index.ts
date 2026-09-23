7a14c08ab6a7e57dc36bdefa45366e57d01d6f15
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-23 20:47:13 +0200

    refactor

--- src/constants/pages/index.ts
+++ src/constants/pages/index.ts
@@ -1,3 +1,1 @@
-export { FEATURED_PROJECT_NAVIGATION_TYPE_TEXT } from './featured.text';
-export { FEATURED_TEXT } from './featured.text';
 export { LANDING_TEXT } from './landing.text';
--- src/projects/pages/featured/components/FeaturedNavigation.tsx
+++ src/projects/pages/featured/components/FeaturedNavigation.tsx
@@ -1,8 +1,8 @@
 import { Link } from 'react-router';
 
-import { FEATURED_PROJECT_NAVIGATION_TYPE_TEXT } from '@/constants/pages';
 import type { FeaturedNavigationItemType } from '@/interfaces';
 import { handleNavigateOnNewWindow } from '@/portfolio/shared';
+import { useTranslation } from 'react-i18next';
 
 interface Props {
   url?: string | null;
@@ -15,16 +15,18 @@ export const FeaturedNavigation = ({ url, type }: Props) => {
   if (!url) return null;
   if (!type) return null;
 
+  const { t } = useTranslation('components');
+
   if (type !== 'page')
     return (
       <div onClick={() => handleNavigateOnNewWindow(url)} className={style}>
-        {FEATURED_PROJECT_NAVIGATION_TYPE_TEXT[type]}
+        {t(`projectCard.navigationType.${type}`)}
       </div>
     );
 
   return (
     <Link className={style} to={url}>
-      {FEATURED_PROJECT_NAVIGATION_TYPE_TEXT[type]}
+      {t(`projectCard.navigationType.${type}`)}
     </Link>
   );
 };
