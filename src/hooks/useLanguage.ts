0754767234edffd72a76eed178ef822f19fa27b8
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-23 17:21:17 +0200

    refactor

--- src/hooks/useLanguage.ts
+++ src/hooks/useLanguage.ts
@@ -1,32 +1,19 @@
-import i18n from '@/i18n/config';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 
-type Language = 'es' | 'en';
-
-const STORAGE_KEY_LANGUAGE = 'portfolio-lang';
+export type Language = 'es' | 'en';
+export const STORAGE_KEY_LANGUAGE = 'portfolio-lang';
 
 export const useLanguage = () => {
-  const [language, setLanguageState] = useState<Language>(() => {
-    const savedLanguage = localStorage.getItem(STORAGE_KEY_LANGUAGE);
+  const { i18n } = useTranslation();
 
-    if (savedLanguage === 'en' || savedLanguage === 'es') {
-      return savedLanguage;
-    }
-
-    return 'en';
-  });
+  const language = i18n.language as Language;
 
   const setLanguage = (language: Language) => {
-    setLanguageState(language);
     i18n.changeLanguage(language);
   };
 
   useEffect(() => {
-    const root = document.documentElement;
-
-    root.classList.remove('es', 'en');
-    root.classList.add(language);
-
     localStorage.setItem(STORAGE_KEY_LANGUAGE, language);
   }, [language]);
 
--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -3,6 +3,11 @@ import { initReactI18next } from 'react-i18next';
 
 import { layout as esLayout } from './es/layout';
 import { layout as enLayout } from './en/layout';
+import { STORAGE_KEY_LANGUAGE } from '@/hooks/useLanguage';
+
+const savedLanguage = localStorage.getItem(STORAGE_KEY_LANGUAGE);
+const initialLanguage =
+  savedLanguage === 'es' || savedLanguage === 'en' ? savedLanguage : 'es';
 
 const resources = {
   es: {
@@ -15,8 +20,8 @@ const resources = {
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: 'en',
-  fallbackLng: 'en',
+  lng: initialLanguage,
+  fallbackLng: 'es',
   interpolation: {
     escapeValue: false,
   },
