e5ea3b135e66516d172416854b2b9c33e1f1801f
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-23 17:47:55 +0200

    implemented i18n at about page

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -3,6 +3,8 @@ import { initReactI18next } from 'react-i18next';
 
 import { layout as esLayout } from './es/layout';
 import { layout as enLayout } from './en/layout';
+import { pages as esPages } from './es/pages';
+import { pages as enPages } from './en/pages';
 import { STORAGE_KEY_LANGUAGE } from '@/hooks/useLanguage';
 
 const savedLanguage = localStorage.getItem(STORAGE_KEY_LANGUAGE);
@@ -12,9 +14,11 @@ const initialLanguage =
 const resources = {
   es: {
     layout: esLayout,
+    pages: esPages,
   },
   en: {
     layout: enLayout,
+    pages: enPages,
   },
 };
 
---
+++ src/i18n/en/pages.ts
@@ -0,0 +1,16 @@
+export const pages = {
+  about: {
+    aboutMe: 'About me',
+    greet: 'Hello and welcome to my portfolio',
+    greetData: [
+      'I have been learning about software development since 2019. What started as something I simply wanted to learn quickly became a hobby, leading me to dedicate more and more time to learning and experimenting with new technologies while enjoying the process.',
+      'After focusing mainly on learning the fundamentals and logic of programming, I am currently interested in taking the next step: learning about software design and architecture. Among the different approaches, Hexagonal Architecture is the one that interests me the most and one of the approaches I try to apply in the projects I develop.',
+      'Although what I enjoy the most is everything related to business logic, especially backend and REST API development, mobile development is another field that I particularly enjoy. Web frontend development is probably the area where I have the most room for improvement, especially when it comes to design, but that is precisely why I also try to keep learning and improving in this area.',
+    ],
+    longTermGoals: 'Long term goals',
+    goalsData: [
+      'I see software development as something that goes beyond writing code. I am interested in understanding how the different parts of a system fit together, from development and architecture to deployment and maintenance.',
+      'That is why, although I currently work with different technologies and I am open to frontend development, my main areas of interest are backend, DevOps, and mobile development. In the long term, I would like to continue expanding my knowledge so that I can take on increasingly greater technical responsibilities, with the goal of eventually working as a Tech Lead or Software Architect.',
+    ],
+  },
+};
