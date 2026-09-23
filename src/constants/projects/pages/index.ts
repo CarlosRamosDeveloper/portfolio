e037f8c66448afe81c9027cf7fdeccad4864f34f
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-23 20:00:46 +0200

    fixed no coincidences bug

--- src/constants/projects/pages/index.ts
+++ src/constants/projects/pages/index.ts
@@ -1,3 +1,2 @@
 export { GALLERY_TEXT } from './gallery.text';
 export { PROJECT_TEXT } from './project.text';
-export { SEARCH_TEXT } from './search.text';
--- src/constants/projects/pages/search.text.ts
+++
@@ -1,7 +0,0 @@
-export const SEARCH_TEXT = {
-  title: 'Búsqueda por tecnologías',
-  navigateToProject: 'Navegar al proyecto',
-  noCoincidences: 'No se han encontrado coincidencias',
-  buttonClean: 'Limpiar filtros',
-  results: 'Resultados de búsqueda',
-};
