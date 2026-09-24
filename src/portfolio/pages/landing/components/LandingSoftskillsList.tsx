ac363c65e9cb9bb2497fbc0c48bf9ce38a3d4f1e
Carlos Ramos <carlosramosdev@protonmail.com>
2026-09-24 12:10:48 +0200

    implemented softskills translation at landing page

--- src/portfolio/pages/landing/components/LandingSoftskillsList.tsx
+++ src/portfolio/pages/landing/components/LandingSoftskillsList.tsx
@@ -1,15 +1,19 @@
 import { Link } from 'react-router';
 import { useTranslation } from 'react-i18next';
 
-import { SoftSkillsData } from '@/data/SoftSkillsData';
 import { LandingSoftskillItem } from './LandingSoftskillItem';
-import { ROUTES } from '@/constants';
+import { ROUTES, SOFT_SKILL_IDS } from '@/constants';
 import { LandingLabel } from './LandingLabel';
-
-const skillsList = [...SoftSkillsData];
 
 export const LandingSoftskillsList = () => {
   const { t } = useTranslation('landing');
+  const { t: ts } = useTranslation('softSkills');
+
+  const softSkills = SOFT_SKILL_IDS.map((id) => ({
+    id,
+    name: ts(`items.${id}.name`),
+    description: ts(`items.${id}.description`),
+  }));
 
   return (
     <div>
@@ -19,7 +23,7 @@ export const LandingSoftskillsList = () => {
         </Link>
       </div>
       <div className="mx-auto grid grid-cols-2 lg:grid-cols-4 gap-3 bg-card p-2 mt-2">
-        {skillsList.map((skill) => (
+        {softSkills.map((skill) => (
           <LandingSoftskillItem key={skill.id} skill={skill} />
         ))}
       </div>
